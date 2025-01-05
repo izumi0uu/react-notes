d7bd7351fc21f27432a5b1fda9b1d9f358ac0919
gao_yangkai <zard200107@gmail.com>
2025-01-05 20:57:56 +0800

    feat: Optimizing server-side rendering -v2

--- app/components/sidebarNoteList/index.jsx
+++ app/components/sidebarNoteList/index.jsx
@@ -1,8 +1,9 @@
 import { getAllNotes } from "@lib/redis";
 import { sleep } from "@lib/utils";
 
-import SidebarNoteItem from "@components/sidebarNoteItem";
+// import SidebarNoteItem from "@components/sidebarNoteItem";
 import SidebarNoteListFilter from "@components/sidebarNoteListFilter";
+import SidebarNoteItemHeader from "@components/sidebarNoteItemHeader";
 
 const SidebarNoteList = async () => {
   await sleep(3000);
@@ -12,11 +13,27 @@ const SidebarNoteList = async () => {
     return <div className="notes-empty">{"No notes created yet!"}</div>;
 
   return (
-    <SidebarNoteListFilter>
-      {Object.entries(notes).map(([id, note]) => {
-        return <SidebarNoteItem noteId={id} note={JSON.parse(note)} />;
+    // <SidebarNoteListFilter>
+    //   {Object.entries(notes).map(([id, note]) => {
+    //     return <SidebarNoteItem noteId={id} note={JSON.parse(note)} />;
+    //   })}
+    // </SidebarNoteListFilter>
+    <SidebarNoteListFilter
+      notes={Object.entries(notes).map(([noteId, note]) => {
+        const noteData = JSON.parse(note);
+        return {
+          noteId,
+          note: noteData,
+          header: (
+            // notice here: which is not as a JSX element, is html element
+            <SidebarNoteItemHeader
+              title={noteData.title}
+              updateTime={noteData.updateTime}
+            />
+          ),
+        };
       })}
-    </SidebarNoteListFilter>
+    />
   );
 };
 
--- app/components/sidebarNoteListFilter/index.jsx
+++ app/components/sidebarNoteListFilter/index.jsx
@@ -1,21 +1,56 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { Children } from "react";
+// import { Children } from "react";
+import SidebarNoteItemContent from "@components/sidebarNoteItemContent";
 
-const SidebarNoteListFilter = ({ children }) => {
+// const SidebarNoteListFilter = ({ children }) => {
+//   const searchParams = useSearchParams();
+//   const searchText = searchParams.get("q");
+
+//   return (
+//     <ul className="notes-list">
+//       {Children.map(children, (child, index) => {
+//         const title = child.props.title;
+//         if (
+//           !searchText ||
+//           (searchText && title.toLowerCase().includes(searchText.toLowerCase()))
+//         ) {
+//           return <li key={index}>{child}</li>;
+//         }
+//         return null;
+//       })}
+//     </ul>
+//   );
+// };
+
+const SidebarNoteListFilter = ({ notes }) => {
   const searchParams = useSearchParams();
   const searchText = searchParams.get("q");
 
   return (
     <ul className="notes-list">
-      {Children.map(children, (child, index) => {
-        const title = child.props.title;
+      {notes.map((noteItem) => {
+        const { noteId, note, header } = noteItem;
         if (
           !searchText ||
-          (searchText && title.toLowerCase().includes(searchText.toLowerCase()))
+          (searchText &&
+            note.title.toLowerCase().includes(searchText.toLowerCase()))
         ) {
-          return <li key={index}>{child}</li>;
+          return (
+            <SidebarNoteItemContent
+              key={noteId}
+              id={noteId}
+              title={note.title}
+              expandedChildren={
+                <p className="sidebar-note-excerpt">
+                  {note.content.substring(0, 20) || <i>(No content)</i>}
+                </p>
+              }
+            >
+              {header}
+            </SidebarNoteItemContent>
+          );
         }
         return null;
       })}
