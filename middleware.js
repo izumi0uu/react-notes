955ef414c2394bb165b83fd80ee5fa3c051769bd
gao_yangkai <zard200107@gmail.com>
2025-01-08 18:41:01 +0800

    feat: middleware for i18n

--- middleware.js
+++ middleware.js
@@ -1,33 +1,41 @@
-import { match } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
+// middleware.js
+import { match } from "@formatjs/intl-localematcher"; // for match language
+import Negotiator from "negotiator"; // for process HTTP
+import { NextResponse } from "next/server";
+
 import { locales, defaultLocale } from "@/config.js";
 
-const getLocale = (request) => {
+function getLocale(request) {
   const headers = {
     "accept-language": request.headers.get("accept-language") || "",
   };
-
   const languages = new Negotiator({ headers }).languages();
 
   return match(languages, locales, defaultLocale);
-};
+}
 
-const middleware = (request) => {
+export function middleware(request) {
+  console.log("middleware", request);
   const { pathname } = request.nextUrl;
+
+  if (pathname.match(/\.(svg|png|jpg|jpeg|gif|webp)$/)) {
+    return NextResponse.next();
+  }
+
+  // 判断请求路径中是否已存在语言，已存在语言则跳过
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
 
-  if (pathnameHasLocale) return;
+  if (pathnameHasLocale) return NextResponse.next();
 
+  // 获取匹配的 locale
   const locale = getLocale(request);
   request.nextUrl.pathname = `/${locale}${pathname}`;
+  // 重定向，如 /products 重定向到 /en-US/products
+  return NextResponse.redirect(request.nextUrl);
+}
 
-  return Response.redirect(request.nextUrl);
-};
-
-const config = {
+export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
-
-export { middleware, config };
